8841a4ec72fc38861191d4a415dead96ddac3322
gabriel80546 <gabriel80546@gmail.com>
2018-03-06 17:30:42 -0300

    gabriel80546

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@
 #/*                 VARIAVEIS                 */
 #/*-------------------------------------------*/
 
-projeto="TCC"
+projeto="tcc"
 CATALINA_HOME="/usr/share/tomcat8"
 CATALINA_BASE="/var/lib/tomcat8"
 
